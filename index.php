29c3168b629f66ea990290f2b2566d7ca5131cdd
Shreyas <smarth.shreyas@gmail.com>
2024-11-06 17:58:33 +0530

    Commiting status testing

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include('includes/config.php');
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="find-result.php">Students</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="find-result.php">Patients</a></li>
                         <li class="nav-item"><a class="nav-link active" href="admin-login.php">Admin</a></li>
                     </ul>
                 </div>
